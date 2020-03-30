6371e0b7216e6cc8e1f31f7ec1c3b99edd09a565
IvanStanchev <48806861+IvanStanchev@users.noreply.github.com>
2020-03-30 12:24:03 +0300

    Update setup.py

--- Person/setup.py
+++ Person/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='PersoN',
         version='1.0.0',
         description='My classwork project',
-        url='https://github.com/IvanStanchev/Skriptovi/blob/master/Person',
+        url='https://github.com/IvanStanchev/Skriptovi/tree/master/Person',
         author='IvanStanchev',
         author_email='ivan.stanchev04@gmail.com',
         license='MIT',
